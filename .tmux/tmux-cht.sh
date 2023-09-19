bea5263585b06d1c3ca02c82454a2cc7d69a9b55
mosfiend <abdellaouimoss@gmail.com>
2023-09-19 14:51:12 +0100

    ansible script

--- .tmux/tmux-cht.sh
+++ .tmux/tmux-cht.sh
@@ -1,15 +1,15 @@
 #usr/bin/env bash
-selected=`cat ~/.tmux/cht-lang ~/.tmux/cht-com | fzf`
+selected=$(cat ~/.tmux/cht-lang ~/.tmux/cht-com | fzf)
 if [[ -z $selected ]]; then
-    exit 0
+	exit 0
 fi
 
 read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/.tmux-cht-languages; then
-    query=`echo $query | tr ' ' '+'`
-    echo $query $selected
-    #tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+	query=$(echo $query | tr ' ' '+')
+	echo $query $selected
+	#tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+	tmux neww -c "curl -s cht.sh/$selected~$query | less"
 fi
